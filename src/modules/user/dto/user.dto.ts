f01bafa24a7210d7ec50f095fd29a149b6a2062d
Vladimir Vaca <ramvlay@gmail.com>
2025-03-14 10:38:46 -0500

    UserDto mapped with open-api decorators

--- src/modules/user/dto/user.dto.ts
+++ src/modules/user/dto/user.dto.ts
@@ -1,5 +1,6 @@
 import { IsNotEmpty, MinLength, IsEmail, IsEnum } from 'class-validator';
 import { User } from '../model/user.model';
+import { ApiProperty } from '@nestjs/swagger';
 
 enum Role {
   ADMIN = 'ADMIN',
@@ -15,20 +16,25 @@ export class UserDto {
     this.role = user.role as Role;
   }
 
+  @ApiProperty({ example: 'Tony' })
   @IsNotEmpty()
   readonly name: string;
 
+  @ApiProperty({ example: 'Stark' })
   @IsNotEmpty()
   readonly lastName: string;
 
+  @ApiProperty({ example: 'tonystark91@gmail.com' })
   @IsNotEmpty()
   @IsEmail()
   readonly email: string;
 
+  @ApiProperty({ example: 'secretpassword' })
   @IsNotEmpty()
   @MinLength(6)
   readonly password: string;
 
+  @ApiProperty({ example: Role.USER, enum: Role })
   @IsNotEmpty()
   @IsEnum(Role, {
     message: 'Role must be either ADMIN or USER',
